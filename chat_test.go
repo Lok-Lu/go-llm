7d24bbcfb2d8113a0bba047b7e60d256b9296e4f
lushijie <lushijie@patsnap.com>
2025-06-13 13:57:41 +0800

    chore: support tgi json format

--- chat_test.go
+++ chat_test.go
@@ -1,48 +1,68 @@
 package go_llm
 
 import (
-	"bytes"
 	"context"
 	"testing"
-	"time"
+
+	"github.com/Lok-Lu/go-llm/general"
 )
 
 func TestChat(t *testing.T) {
 	url := ""
-	client := NewClient().WithGeneralClient(url)
-	// var (
-	// 	a   float64 = 1.0
-	// 	b   float64 = 0.6
-	// 	c   float64 = 1.3
-	// 	d   int     = 1024
-	// 	req         = general.ChatRequest{
-	// 		Inputs: "你是谁[AI]",
-	// 		Parameters: general.ChatParams{
-	// 			MaxNewTokens:      1024,
-	// 			Temperature:       &a,
-	// 			TopK:              &d,
-	// 			TopP:              &b,
-	// 			RepetitionPenalty: &c,
-	// 			Truncate:          &d,
-	// 		},
-	// 	}
-	// )
-	// chat, err := client.Llm.CreateChat(context.Background(), url, &req)
-	// if err != nil {
-	// 	t.Error(err)
-	// }
-	//for {
-	//	r, err := chat.Recv()
-	//	if err != nil {
-	//		break
-	//	}
-	//	t.Log(r)
-	//}
-	metric, err := client.Llm.GetMetrics(context.Background(), url, time.Second*3)
+	client := NewClient().WithGeneralClient(url, "")
+	var (
+		a float64 = 1.0
+		// b   float64 = 0.6
+		// c   float64 = 1.3
+		// d   int     = 1024
+		req = general.ChatRequest{
+			Inputs: "convert to JSON: I saw a puppy a cat and a raccoon during my bike ride in the park, use this json schema",
+			Parameters: general.ChatParams{
+				MaxNewTokens: 1024,
+				Temperature:  &a,
+				// TopK:              &d,
+				// TopP:              &b,
+				// RepetitionPenalty: &c,
+				// Truncate:          &d,
+				Grammar: &general.GrammarParams{
+					Type: "json",
+					Value: general.SchemaParams{
+						Properties: map[string]general.Definition{
+							"location": {"title": "Location", "type": "string"},
+							"activity": {"title": "Activity", "type": "string"},
+							"animals_seen": {
+								"maximum": 5,
+								"minimum": 1,
+								"title":   "Animals Seen",
+								"type":    "integer",
+							},
+							"animals": {"title": "Animals", "type": "array", "items": map[string]any{"type": "string"}},
+						},
+						Required: []string{"location", "activity", "animals_seen", "animals"},
+						Title:    "Animals",
+						Type:     "object",
+					},
+				},
+			},
+		}
+	)
+	chat, err := client.Llm.CreateChat(context.Background(), url, &req)
 	if err != nil {
 		t.Error(err)
 	}
-	a := bytes.Split(metric, []byte("\n"))
-	t.Log(string(a[0]))
+	t.Logf("%+v", chat)
+	// for {
+	// 	r, err := chat.Recv()
+	// 	if err != nil {
+	// 		break
+	// 	}
+	// 	t.Log(r)
+	// }
+	// metric, err := client.Llm.GetMetrics(context.Background(), url, time.Second*3)
+	// if err != nil {
+	// 	t.Error(err)
+	// }
+	// a := bytes.Split(metric, []byte("\n"))
+	// t.Log(string(a[0]))
 
 }
--- general/types.go
+++ general/types.go
@@ -1,21 +1,57 @@
 package general
 
+type DataType string
+
+const (
+	Object  DataType = "object"
+	Number  DataType = "number"
+	Integer DataType = "integer"
+	String  DataType = "string"
+	Array   DataType = "array"
+	Null    DataType = "null"
+	Boolean DataType = "boolean"
+)
+
+type GrammarType string
+
+const (
+	Json       GrammarType = "json"
+	JsonSchema GrammarType = "json_schema"
+	Regex      GrammarType = "regex"
+)
+
+type GrammarParams struct {
+	Type  GrammarType  `json:"type"`
+	Value SchemaParams `json:"value"`
+}
+
+type Definition map[string]any
+
+type SchemaParams struct {
+	Properties map[string]Definition `json:"properties,omitempty"`
+	Required   []string              `json:"required,omitempty"`
+	Title      string                `json:"title,omitempty"`
+	Type       DataType              `json:"type,omitempty"`
+	Items      Definition            `json:"items,omitempty"`
+}
+
 type ChatParams struct {
-	MaxNewTokens      int      `json:"max_new_tokens"`
-	Temperature       *float64 `json:"temperature,omitempty"`
-	TopK              *int     `json:"top_k,omitempty"`
-	TopP              *float64 `json:"top_p,omitempty"`
-	Seed              *int     `json:"seed,omitempty"`
-	Stop              []string `json:"stop,omitempty"`
-	DoSample          *bool    `json:"do_sample,omitempty"`
-	RepetitionPenalty *float64 `json:"repetition_penalty,omitempty"`
-	BestOf            *int     `json:"best_of,omitempty"`
-	Details           *bool    `json:"details,omitempty"`
-	ReturnFullText    *bool    `json:"return_full_text,omitempty"`
-	Truncate          *int     `json:"truncate,omitempty"`
-	TypicalP          *int     `json:"typical_p,omitempty"`
-	WaterMark         *bool    `json:"watermark,omitempty"`
-	NumBeams          *int     `json:"num_beams,omitempty"`
+	MaxNewTokens      int            `json:"max_new_tokens"`
+	Temperature       *float64       `json:"temperature,omitempty"`
+	TopK              *int           `json:"top_k,omitempty"`
+	TopP              *float64       `json:"top_p,omitempty"`
+	Seed              *int           `json:"seed,omitempty"`
+	Stop              []string       `json:"stop,omitempty"`
+	DoSample          *bool          `json:"do_sample,omitempty"`
+	RepetitionPenalty *float64       `json:"repetition_penalty,omitempty"`
+	BestOf            *int           `json:"best_of,omitempty"`
+	Details           *bool          `json:"details,omitempty"`
+	ReturnFullText    *bool          `json:"return_full_text,omitempty"`
+	Truncate          *int           `json:"truncate,omitempty"`
+	TypicalP          *int           `json:"typical_p,omitempty"`
+	WaterMark         *bool          `json:"watermark,omitempty"`
+	NumBeams          *int           `json:"num_beams,omitempty"`
+	Grammar           *GrammarParams `json:"grammar,omitempty"`
 }
 
 type ChatRequest struct {
